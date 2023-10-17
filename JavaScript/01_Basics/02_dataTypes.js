d0fb190cbc48b3549e77b4254c800d006fb795a9
Omgupta050 <squadxgamerz@gmail.com>
2023-10-17 18:46:56 +0530

    02_Variabled

---
+++ JavaScript/01_Basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all JS code as newer JS version
+
+// alert ( 3 + 3 ) // we are using nodejs, not browser
+
+// console.log( 3 + 3 );
+
+// console.log("Om Gupta");
+
+// code readability should be high
+
+let name = "Om";
+let age = 18;
+let isLoggedIn = False;
+
+// number => 2 to power 53
+// BigInt
+// string => ""
+// Boolean => true/false
+// null => standalone value
+// undefined
+// symbol => unique
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
