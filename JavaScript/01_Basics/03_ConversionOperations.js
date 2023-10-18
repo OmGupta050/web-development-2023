06fa33fc91b7c91e37a2a460d3f128c433445423
omgupta05 <omgupta0509@gmail.com>
2023-10-18 12:06:01 +0530

    03_Conversions

---
+++ JavaScript/01_Basics/03_ConversionOperations.js
@@ -0,0 +1,36 @@
+// let score = "33abc"
+// let score = "33"
+let score = 33
+// let score = null
+// let score = undefined
+// let score = true
+// let score = false
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = 1
+// let isLoggedIn = ""
+// let isLoggedIn = "Om"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "Om" => true
+
+let someNumber = 1708
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
