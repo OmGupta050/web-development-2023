81cc7becba518cc570a5cc60431ee518596bf09a
omgupta05 <omgupta0509@gmail.com>
2023-10-20 11:51:39 +0530

    03_Operations

--- JavaScript/01_Basics/03_ConversionOperations.js
+++ JavaScript/01_Basics/03_ConversionOperations.js
@@ -34,3 +34,41 @@ let someNumber = 1708
 let stringNumber = String(someNumber)
 // console.log(stringNumber);
 // console.log(typeof stringNumber);
+
+// ******************************* Operations *******************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2%2);
+// console.log(2**3);
+
+let str1 = "Hello "
+let str2 = "Om"
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(5 + 3 - 9 / 2 % 8 * 7);
+// console.log((5 + 3) - (9 / 2) % (8 * 7));
+// console.log((5 + 3) - 9 / 2 % (8 * 7));
+// console.log((5 + 3) - 9 /( 2 % 8) * 7);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2;
+
+let gameCounter = 100
+gameCounter++;
+// console.log(gameCounter);
