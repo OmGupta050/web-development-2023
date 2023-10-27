384ac974490233186f9e3ca5812014fee8d7fd52
omgupta05 <omgupta0509@gmail.com>
2023-10-27 16:30:34 +0530

    05_String

---
+++ JavaScript/01_Basics/05_String.js
@@ -0,0 +1,34 @@
+const name = "Om";
+const repoCount = 50;
+
+// console.log(name + repoCount + Value);
+
+// console.log(`Hello my name is ${name} and my repo is ${repoCount}`);
+
+const gameName = new String("Omguptaog");
+
+// console.log(gameName[0] );
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase() );
+// console.log(gameName.charAt(4) );
+// console.log(gameName.indexOf('t') );
+
+const newString = gameName.substring(0, 5);
+// console.log(newString);
+
+const anotherString = gameName.slice(0, 5);
+// console.log(anotherString);
+
+const newStringOne = "       Om       ";
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+const url = "https://om.com/om%20gupta"
+
+// console.log(url.replace('%20', '-'));
+
+// console.log(url.includes('sundar'));
+
+// console.log(gameName.split('-'));
