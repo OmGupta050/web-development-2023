7a9985061fa7ddde21267d77a264211364e58690
omgupta05 <omgupta0509@gmail.com>
2023-10-27 16:09:41 +0530

    Stack and Heap Memory

--- JavaScript/dataTypes-Summary.js
+++ JavaScript/dataTypes-Summary.js
@@ -29,3 +29,27 @@ let myObj = {
 const myFunction = function(){
     // console.log("Hello World");
 }
+
+// console.log(typeof myFunction);
+
+//************************************* Stack and Heap Memory **************************************
+
+// Stack (Primitive), Heap (Non-Primitive)
+
+let myEmailid = "omg@gmail.com";
+let anotherEmailid = myEmailid;
+anotherEmailid = "gom@gmail.com";
+
+// console.log(anotherEmailid);
+// console.log(myEmailid);
+
+let userOne = {
+    email: "ogm@gmail.com",
+    upi: "ogm@icici.upi"
+}
+
+let userTwo = userOne;
+userTwo.email = "mog@gmail.com"
+
+// console.log(userOne.email);
+// console.log(userTwo.email);
