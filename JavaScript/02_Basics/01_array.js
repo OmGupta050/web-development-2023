3fada7722e3b3af62ee556fad14b0297d925ec2f
OmGupta050 <squadxgamerz@gmail.com>
2023-10-31 12:02:40 +0530

    02_Basics/01_array

---
+++ JavaScript/02_Basics/01_array.js
@@ -0,0 +1,36 @@
+const myArr = [0, 1, 2, 3, 4, 5];
+const myHeros = ["Ironman", "Spiderman", "Shaktiman"];
+
+const myArr2 = new Array(1, 2, 3, 4);
+// console.log(myArr[1]);
+
+// Array Methods
+
+myArr.push(6);
+myArr.push(7);
+myArr.pop();
+
+myArr.unshift(-1);
+myArr.shift();
+
+// console.log(myArr.includes(8));
+// console.log(myArr.indexOf(8));
+
+// console.log(myArr);
+
+const newArr = myArr.join();
+// console.log(myArr);
+// console.log(newArr);
+// console.log(typeof newArr);
+
+// Slice, Splice
+
+// console.log("A ", myArr);
+
+const myN1 = myArr.slice(1, 3);
+// console.log(myN1);
+// console.log("B ", myArr);
+
+const myN2 = myArr.splice(1, 3);
+// console.log(myN2);
+// console.log("C ", myArr);
