13ce8b59b4e5a241faeecab182d46864a2ed7b15
OmGupta050 <squadxgamerz@gmail.com>
2023-11-22 17:40:07 +0530

    03_Basics/01_functions

---
+++ JavaScript/03_Basics/01_functions.js
@@ -0,0 +1,37 @@
+// console.log("O");
+// console.log("M");
+
+function sayMyName(){
+    // console.log("O");
+    // console.log("M");
+}
+
+// sayMyName()
+
+// function addTwoNumbers(number1, number2){
+    // console.log(number1 + number2);
+// }
+
+
+function addTwoNumbers(number1, number2){
+    
+    // let result = number1 + number2
+    // return result
+
+    // return number1 + number2
+}
+
+const result = addTwoNumbers(3, 5);
+
+// console.log("Result: ", result);
+
+function loginUserMessage(username = "Paul"){
+    if(!username){
+        // console.log("Please enter a username");
+        return
+    }
+    return `${username} just logged in`
+}
+
+// console.log(loginUserMessage("Om"));
+// console.log(loginUserMessage("Om"));
