c68e5c05cd4ecbd9e26cd37b0371575521dda7d0
OmGupta050 <squadxgamerz@gmail.com>
2023-11-24 11:29:11 +0530

    03_Basics/01_function Continue

--- JavaScript/03_Basics/01_functions.js
+++ JavaScript/03_Basics/01_functions.js
@@ -35,3 +35,34 @@ function loginUserMessage(username = "Paul"){
 
 // console.log(loginUserMessage("Om"));
 // console.log(loginUserMessage("Om"));
+
+function calculateCartPrice(val1, val2, ...num1){
+    return num1
+}
+
+// console.log(calculateCartPrice(200, 400, 500, 2000));
+
+const user = {
+    username: "Om",
+    price: 199
+}
+
+function handleObject(anyObject){
+    // console.log(`username is ${anyObject.username} and price is ${anyObject.price}`);
+}
+
+// handleObject(user)
+
+handleObject({
+    username: "Paul",
+    price: 499
+})
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray));
+// console.log(returnSecondValue([200, 400, 500, 1000]));
